b014689580f3be319b7fb8b2b2f4cc363dd2528a
Edmund Miller <git@edmundmiller.dev>
2024-10-08 18:39:22 -0500

    Remove months, because there's no much happening

--- queries/state.sql
+++ queries/state.sql
@@ -4,6 +4,6 @@ select
 from legiscan.bills
 where state not in ('United States', 'New York City')
   and extract(year from last_action_date) = 2024
-  and extract(month from last_action_date) = 4
+--   and extract(month from last_action_date) = 4
 group by state
 order by state_name
