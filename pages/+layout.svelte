1d0d9417335bc85453cb96f405285a1e2a1f3075
Edmund Miller <git@edmundmiller.dev>
2024-10-08 22:16:45 -0500

    Add periodic logo

---
+++ pages/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import '@evidence-dev/tailwind/fonts.css';
+	import '../app.css';
+	import { EvidenceDefaultLayout } from '@evidence-dev/core-components';
+	export let data;
+</script>
+
+<EvidenceDefaultLayout {data} githubRepo=https://github.com/edmundmiller/periodic_legislation
+logo=/periodic_logo.jpg>
+	<slot slot="content" />
+</EvidenceDefaultLayout>
