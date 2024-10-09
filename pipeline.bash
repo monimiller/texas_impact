2b8d8ca7df10272f229913b53a80a4af49ed5461
Edmund Miller <git@edmundmiller.dev>
2024-10-08 21:32:09 -0500

    chore: Refactor data pipeline to handle API pagination

--- pipeline.bash
+++ pipeline.bash
@@ -2,35 +2,46 @@
 
 APIKEY="915b8d36efc1524035bf6336561b07e6"
 # Fetch bills related to Feminine Hygiene from LegiScan API
-curl -G "https://api.legiscan.com/" \
-    --data-urlencode "key=$APIKEY" \
-    --data-urlencode "op=getSearch" \
-    --data-urlencode "state=ALL" \
-    --data-urlencode "year=4" \
-    --data-urlencode "query=Feminine Hygiene" |
-    jq -c '
-    .searchresult |
-    to_entries[] |
-    select(.key | tonumber? != null) |
-    .value |
-    {
-        state,
-        bill_number,
-        bill_id,
-        url,
-        text_url,
-        last_action_date,
-        last_action,
-        title
-    }
-' | duckdb -c "
-    CREATE TABLE new_bills AS
-    SELECT * FROM read_json_auto('/dev/stdin');
-    
-    CREATE TABLE IF NOT EXISTS bills AS
-    SELECT * FROM 'sources/legiscan/bills.csv';
-    
-    INSERT INTO bills SELECT * FROM new_bills;
+page=1
+while true; do
+    data=$(curl -G "https://api.legiscan.com/" \
+        --data-urlencode "key=$APIKEY" \
+        --data-urlencode "op=getSearch" \
+        --data-urlencode "state=ALL" \
+        --data-urlencode "year=4" \
+        --data-urlencode "page=$page" \
+        --data-urlencode "query=Feminine Hygiene" | jq -c '
+            .searchresult |
+            to_entries[] |
+            select(.key | tonumber? != null) |
+            .value |
+            {
+                state,
+                bill_number,
+                bill_id,
+                url,
+                text_url,
+                last_action_date,
+                last_action,
+                title
+            }
+        ')
 
-    COPY bills TO 'sources/legiscan/bills.csv' (HEADER, DELIMITER ',');
-"
+    if [ -z "$data" ]; then
+        break
+    fi
+
+    echo "$data" | duckdb -c "
+        CREATE TABLE new_bills AS
+        SELECT * FROM read_json_auto('/dev/stdin');
+        
+        CREATE TABLE IF NOT EXISTS bills AS
+        SELECT * FROM 'sources/legiscan/bills.csv';
+        
+        INSERT INTO bills SELECT * FROM new_bills;
+    
+        COPY bills TO 'sources/legiscan/bills.csv' (HEADER, DELIMITER ',');
+    "
+
+    page=$((page + 1))
+done
