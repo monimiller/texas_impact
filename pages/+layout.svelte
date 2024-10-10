2b553a27bbc9dfad237fc8a5c0ffbd6529dd3a00
Edmund Miller <git@edmundmiller.dev>
2024-10-10 14:05:52 -0500

    Try to remove "Evidence" tab title

--- pages/+layout.svelte
+++ pages/+layout.svelte
@@ -1,11 +1,16 @@
 <script>
-	import '@evidence-dev/tailwind/fonts.css';
-	import '../app.css';
-	import { EvidenceDefaultLayout } from '@evidence-dev/core-components';
+	import "@evidence-dev/tailwind/fonts.css";
+	import "../app.css";
+	import { EvidenceDefaultLayout } from "@evidence-dev/core-components";
 	export let data;
 </script>
 
-<EvidenceDefaultLayout {data} githubRepo=https://github.com/edmundmiller/periodic_legislation
-logo=/periodic_logo.jpg>
+<EvidenceDefaultLayout
+	{data}
+	githubRepo="https://github.com/edmundmiller/periodic_legislation"
+	logo="/periodic_logo.jpg"
+	builtWithEvidence="true"
+	title="Periodic Legislation Tracker"
+>
 	<slot slot="content" />
 </EvidenceDefaultLayout>
