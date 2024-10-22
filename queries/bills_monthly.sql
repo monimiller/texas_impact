d4e4a963e09b04e4653457c55aabca1c031e8406
Edmund Miller <git@edmundmiller.dev>
2024-10-21 21:30:21 -0500

    Call it total_bills_actions

--- queries/bills_monthly.sql
+++ queries/bills_monthly.sql
@@ -1,6 +1,6 @@
 SELECT
   last_action_date,
-  COUNT(DISTINCT bill_number) AS total_bills,
+  COUNT(DISTINCT bill_number) AS total_bills_actions,
   SUM(COUNT(DISTINCT bill_number)) OVER (ORDER BY last_action_date ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS rolling_total_bills
 FROM legiscan.bills
 GROUP BY last_action_date
