e66746f1e5d11952974fe2c23cef139f15a69b34
Edmund Miller <git@edmundmiller.dev>
2024-12-18 21:20:12 -0600

    fix show all time count when selecting date

--- queries/bills_monthly.sql
+++ queries/bills_monthly.sql
@@ -1,18 +1,23 @@
-WITH daily_counts AS (
+WITH all_time_totals AS (
   SELECT
     last_action_date::date AS day,
     COUNT(DISTINCT bill_id) as total_bills
   FROM legiscan.bills
-  WHERE 
-    last_action_date >= '${inputs.selected_timeframe.start}'
-    AND last_action_date <= '${inputs.selected_timeframe.end}'
   GROUP BY last_action_date::date
+),
+filtered_days AS (
+  SELECT
+    day,
+    total_bills,
+    SUM(total_bills) OVER (ORDER BY day) as rolling_total_bills
+  FROM all_time_totals
 )
 SELECT
   day,
   total_bills,
-  SUM(total_bills) OVER (
-    ORDER BY day
-  ) as rolling_total_bills
-FROM daily_counts
+  rolling_total_bills
+FROM filtered_days
+WHERE 
+  day >= '${inputs.selected_timeframe.start}'
+  AND day <= '${inputs.selected_timeframe.end}'
 ORDER BY day;
