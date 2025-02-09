7f51b6e3a71a0b3e85ddee3dd7dbb81ab7eed0e2
Edmund Miller <git@edmundmiller.dev>
2025-02-09 14:19:01 -0600

    fix: Update the query

--- queries/bills.sql
+++ queries/bills.sql
@@ -1,9 +1,14 @@
-select
-    number,
-    url,
-    status_date,
-    status,
-    last_action_date,
-    last_action,
-    title
-from legiscan.monitored_bills;
+SELECT 
+
+    l.number,
+    t.topic,
+    t.position,
+    t.description as texas_impact_description,
+    l.url,
+    l.status_date,
+    l.status,
+    l.last_action_date,
+    l.last_action,
+    l.title
+from legiscan.monitored_bills l
+LEFT JOIN texas_impact.bills t ON l.number = t.bill_number;
